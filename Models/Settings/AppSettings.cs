43149244bb084842ac15760aca1fb2ef78b9f863
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-05-23 14:15:06 -0500

    Add project files.

---
+++ Models/Settings/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieProDemo.Models.Settings
+{
+  public class AppSettings
+  {
+    public  MovieProSettings MovieProSettings { get; set; }
+    public  TMDBSettings TMDBSettings { get; set; }
+  }
+}
