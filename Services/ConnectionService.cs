137b23b5a2a5a2d288295f6603d57143bff8cd6f
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-05-24 00:42:42 -0500

    Wrote connection service to generate connection string for npgsql

---
+++ Services/ConnectionService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieProDemo.Services
+{
+  //builds connection string to communicate with database...dont use to many static classes due to using long term heap memory
+  public class ConnectionService
+  {
+    public static string GetConnectionString(IConfiguration configuration)
+    {
+      //this may need to be altered to work with user secrets
+      var connectionString = configuration.GetConnectionString("DefaultConnection");
+      var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+      return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+    }
+
+    private static string BuildConnectionString(string databaseUrl)
+    {
+      //does best to take in database url string and turn it into a viable connection string
+      //incoming url string is turned into an instantce of the uri class
+      //because this is static it doesn't have to be registered as a service 
+      
+      var databaseUri = new Uri(databaseUrl);
+      var userInfo = databaseUri.UserInfo.Split(';');
+      var builder = new NpgsqlConnectionStringBuilder
+      {
+        Host = databaseUri.Host,
+        Port = databaseUri.Port,
+        Username = userInfo[0],
+        Password = userInfo[1],
+        Database = databaseUri.LocalPath.TrimStart('/'),
+        SslMode = SslMode.Require,
+        TrustServerCertificate = true
+
+
+      };
+      return builder.ToString();
+    }
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MovieProDemo.Data;
 using MovieProDemo.Models.Settings;
+using MovieProDemo.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +31,7 @@ namespace MovieProDemo
     {
       services.AddDbContext<ApplicationDbContext>(options =>
           options.UseNpgsql(
-              Configuration.GetConnectionString("DefaultConnection")));
+              ConnectionService.GetConnectionString(Configuration)));
       services.AddDatabaseDeveloperPageExceptionFilter();
 
       services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
