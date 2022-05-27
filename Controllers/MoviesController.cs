70bb50e253b1c604299e5bdef1e5a8fd8e653036
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-05-27 00:39:35 -0500

    created crud for movies controller via temporary controller

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using MovieProDemo.Data;
@@ -71,6 +72,141 @@ namespace MovieProDemo.Controllers
       var movies = await _context.Movie.ToListAsync();
       return View(movies);
     }
+
+    // GET: Temporary/Create
+    public IActionResult Create()
+    {
+      ViewData["CollectionId"] = new SelectList(_context.Collection, "Id", "Name");
+      return View();
+    }
+
+    // POST: Temporary/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,MovieId,Title,TagLine,Overview,RunTime,ReleaseDate,Rating,VoteAverage,Poster,PosterType,Backdrop,BackdropType,TrailerUrl")] Movie movie, int collectionId)
+    {
+      if (ModelState.IsValid)
+      {
+
+        movie.PosterType = movie.PosterFile?.ContentType;
+        movie.Poster = await _imageService.EncodeImageAsync(movie.PosterFile);
+
+        movie.BackdropType = movie.BackdropFile?.ContentType;
+        movie.Backdrop = await _imageService.EncodeImageAsync(movie.BackdropFile);
+
+        _context.Add(movie);
+        await _context.SaveChangesAsync();
+
+        await AddToMovieCollection(movie.Id, collectionId);
+
+        return RedirectToAction("Index", "MovieCollections");
+      }
+      return View(movie);
+    }
+
+    // GET: Temporary/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var movie = await _context.Movie.FindAsync(id);
+      if (movie == null)
+      {
+        return NotFound();
+      }
+      return View(movie);
+    }
+
+    // POST: Temporary/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,MovieId,Title,TagLine,Overview,RunTime,ReleaseDate,Rating,VoteAverage,Poster,PosterType,Backdrop,BackdropType,TrailerUrl")] Movie movie)
+    {
+      if (id != movie.Id)
+      {
+        return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          if(movie.PosterFile is not null)
+          {
+            movie.PosterType = movie.PosterFile?.ContentType;
+            movie.Poster = await _imageService.EncodeImageAsync(movie.PosterFile);
+          }
+          if(movie.BackdropFile is not null)
+          {
+            movie.BackdropType = movie.BackdropFile?.ContentType;
+            movie.Backdrop = await _imageService.EncodeImageAsync(movie.BackdropFile);
+          }
+
+          _context.Update(movie);
+          await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!MovieExists(movie.Id))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        //details action of movies controller with route of movie id
+        return RedirectToAction("Details", "Movies", new { id = movie.Id, local = true });
+      }
+      return View(movie);
+    }
+
+    // GET: Temporary/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var movie = await _context.Movie
+          .FirstOrDefaultAsync(m => m.Id == id);
+      if (movie == null)
+      {
+        return NotFound();
+      }
+
+      return View(movie);
+    }
+
+    // POST: Temporary/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+      var movie = await _context.Movie.FindAsync(id);
+      _context.Movie.Remove(movie);
+      await _context.SaveChangesAsync();
+      return RedirectToAction("Library", "Movies");
+    }
+
+    private bool MovieExists(int id)
+    {
+      return _context.Movie.Any(e => e.Id == id);
+    }
+
+
+
+
+
 
     private async Task AddToMovieCollection(int movieId, string collectionName)
     {
